2878a9981d51c5a23f1838827b9ac23d6f35b1bc
Gerrard Xiong <chengcheng.xiong@my.jcu.edu.au>
2023-07-13 21:25:06 +1000

    create RankingGrid

--- RankingApp/ClientApp/src/components/RankItems.js
+++ RankingApp/ClientApp/src/components/RankItems.js
@@ -1,6 +1,7 @@
 ﻿
 import React, { useState, useEffect } from 'react';
 import MovieImageArr from './MovieImages.js';
+import RankingGrid from './RankingGrid.js';
 
 
 const RankItems = () => {
@@ -21,12 +22,13 @@ const RankItems = () => {
 
     return (
         <main>
+            <RankingGrid items={items} imgArr={MovieImageArr} />
             <div className='items-not-ranked'>
                 {
                     (items.length > 0) ? items.map((item) =>
                         <div className='unranked-cell'>
-                        <img id={`item-${item.id}`} src={MovieImageArr.find(o=>o.id === item.imageId)?.image} />
-                    </div>
+                            <img id={`item-${item.id}`} src={MovieImageArr.find(o => o.id === item.imageId)?.image} />
+                        </div>
                     ) : <div>Loading...</div>
                 }
             </div>
